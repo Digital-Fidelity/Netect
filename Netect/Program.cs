ed32cb6347413406d415be080d08c50e117b5b4c
Digital Fidelity <admin@digitalfidelity.co.za>
2023-08-17 16:37:18 +0200

    Netect
    This project is a basic Network Traffic Analyzer
    written in C#. With the ability to capture and analyze network traffic
    it presents users with visibility into various data exchanges occurring on their network.
    This open-source tool allows users to monitor incoming and outgoing data packets
    identifying any suspicious network traffic in real-time.
    
    Key Features:
    
    Traffic Capture: Captures network traffic data, allowing users to inspect different layers of communication protocols.
    Real-time Analysis: Provides immediate analysis of network traffic, detecting any potential threats or suspicious activities.
    Alerts and Notifications: Notifies users of any identified suspicious network traffic or patterns, ensuring prompt attention to potential security risks.
    Intuitive Dashboard: Presents analyzed data in a user-friendly dashboard to give a comprehensive overview of network traffic patterns.
    Customization: Allows users to define their own criteria for identifying suspicious activities, tailoring the tool to specific network monitoring needs.
    Whether you are a system administrator, security analyst, or a curious developer, this project offers a foundation for building a more robust network
    monitoring and visualization system. Contribute to this open-source project, expand its capabilities, and drive continuous improvements in network security.

---
+++ Netect/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Linq;
+using System.Media;
+using System.Net.NetworkInformation;
+using System.Threading;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Monitoring for suspicious network traffic...");
+
+        NetworkChange.NetworkAddressChanged += (_, __) =>
+        {
+            ThreadPool.QueueUserWorkItem(_ =>
+            {
+                CheckForSuspiciousTraffic();
+            });
+        };
+
+        Console.ReadLine();
+    }
+
+    static void CheckForSuspiciousTraffic()
+    {
+        var interfaces = NetworkInterface.GetAllNetworkInterfaces()
+            .Where(i => i.OperationalStatus == OperationalStatus.Up);
+
+        foreach (var networkInterface in interfaces)
+        {
+            var statistics = networkInterface.GetIPv4Statistics();
+
+            // Check for suspicious criteria
+            if (statistics.UnicastPacketsReceived == 0)
+            {
+                HighlightAndAlert(networkInterface.Name);
+            }
+        }
+    }
+
+    static void HighlightAndAlert(string message)
+    {
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine($"{DateTime.Now}: Suspicious network traffic detected: {message}");
+        Console.ResetColor();
+
+        // Play alert sound
+        using (var soundPlayer = new SoundPlayer(@"alarm.mp3"))
+        {
+            soundPlayer.Play();
+        }
+    }
+}
